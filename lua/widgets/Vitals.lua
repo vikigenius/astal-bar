abdf971ce49776a03ddd79a9789875b47ec92993
Braian A. Diez <bdiez19@gmail.com>
2025-02-08 16:20:26 -0300

    style(vitals): add styles to vitals widgets

--- lua/widgets/Vitals.lua
+++ lua/widgets/Vitals.lua
@@ -7,7 +7,7 @@ local function CpuWidget()
   local vitals = Vitals.get_default()
 
   return Widget.Box({
-    class_name = "VitalsCpu",
+    class_name = "cpu",
     Widget.Icon({
       icon = os.getenv("PWD") .. "/icons/cpu-symbolic.svg",
       css = "padding-right: 5pt;",
@@ -24,7 +24,7 @@ local function MemoryWidget()
   local vitals = Vitals.get_default()
 
   return Widget.Box({
-    class_name = "VitalsMemory",
+    class_name = "memory",
     Widget.Icon({
       icon = os.getenv("PWD") .. "/icons/memory-symbolic.svg",
       css = "padding-right: 5pt;",
@@ -41,6 +41,7 @@ local function VitalsWidget()
   return Widget.Box({
     css = "padding: 0 5pt;",
     class_name = "Vitals",
+    spacing = 5,
     MemoryWidget(),
     CpuWidget(),
   })
